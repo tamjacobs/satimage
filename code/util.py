25310af77ccfed1775f7fc211d91bded831657ff
TamJacobs <tamar.jacobs@live.com>
2020-07-19 18:18:26 +1000

    Tensorflow version modification

--- code/util.py
+++ code/util.py
@@ -7,8 +7,7 @@ import pylab as pl
 #import tensorflow.compat.v1 as tf
 #tf.disable_v2_behavior()
 #from tf.keras.models import model_from_json
-import tensorflow as tf
-from tf.keras.models import model_from_json
+from tensorflow.keras.model import model_from_json
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
